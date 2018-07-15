38195a8a9fd5c5735408f281c249e076506e925b
Tim Jaroch <jarocht@mail.gvsu.edu>
2018-07-15 17:27:37 -0400

    Spun up angular. Implemented routes and multiple controllers.

--- site/js/index.js
+++ site/js/index.js
@@ -1,25 +1,116 @@
-var baseUrl = "http://david:3000/api/sprinkler";
+var baseUrl = "http://tiberius.local:3000/api/sprinkler";
 var modes;
+
+var tiberiusApp = angular.module('tiberiusApp', ['ngRoute']);
+
+tiberiusApp.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
+        })
+        .when('/thermostat', {
+            templateUrl: 'pages/thermostat.html',
+            controller: 'thermostatController'
+        })
+        .when('/sprinkler', {
+            templateUrl: 'pages/sprinkler.html',
+            controller: 'sprinklerController'
+        })
+        .when('/sensors', {
+            templateUrl: 'pages/sensors.html',
+            controller: 'sensorsController'
+        })
+        .otherwise({
+            templateUrl: 'pages/home.html'
+        });
+
+    $locationProvider.hashPrefix('');
+
+});
+
+tiberiusApp.controller('mainController', function ($scope, $http, $interval) {
+    $scope.Model = {
+        dataLoaded: false,
+     };
+    $interval(() => {
+        $http({
+            method: 'GET',
+            url: 'http://tiberius.local:3000/api/status'
+        }).then(function successCallback(response) {
+            $scope.status = response.data;
+            $scope.Model.dataLoaded = (new Date).getTime();
+            // this callback will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }, 2000);
+});
+
+tiberiusApp.controller('homeController', function ($scope) {
+    $scope.message = "Welcome to Tiberius";
+});
+
+tiberiusApp.controller('thermostatController', function ($scope, $http) {
+    $scope.message = "Thermostat Page";
+    $scope.$watch('Model.dataLoaded', function(dataLoaded) {
+        if (dataLoaded) {
+            $scope.temperature = ($scope.$parent.status.sensor.fahrenheit.value).toFixed(1) + $scope.$parent.status.sensor.fahrenheit.unit;
+            $scope.humidity = ($scope.$parent.status.sensor.humidity.value).toFixed(1) + $scope.$parent.status.sensor.humidity.unit;
+        }
+      });
+});
+
+tiberiusApp.controller('sensorsController', function ($scope) {
+    $scope.message = "Sensors Page";
+});
+
+tiberiusApp.controller('sprinklerController', function ($scope) {
+    $scope.message = 'Sprinkler Page!';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function startWatch() {
     getState();
-    setInterval(function(){ 
-        getState(); 
+    setInterval(function () {
+        getState();
     }, 100);
 }
 
 function getState() {
-    get(baseUrl + "status", function(data){
+    get(baseUrl + "status", function (data) {
         modes = data.modes;
         for (var i = 0; i < data.pinCount; i++) {
             var element = document.getElementById(i);
-            if (element)
-            {
+            if (element) {
                 if (data.pinStatus[i] == 0) {
                     element.classList.remove('btn-primary');
                     element.classList.add('btn-success');
-                } else {          
+                } else {
                     element.classList.remove('btn-success');
                     element.classList.add('btn-primary');
                 }
@@ -56,7 +147,7 @@ function toggleSidebar() {
 
 function toggle(id) {
     var element = document.getElementById(id);
-    get(baseUrl + "manual/" + id, function(response) {
+    get(baseUrl + "manual/" + id, function (response) {
         if (!response.success) {
             $('#myModal').modal('show');
         }
@@ -65,7 +156,7 @@ function toggle(id) {
 
 function toggleMode(id) {
     var element = document.getElementById(id);
-    get(baseUrl + "set/" + id, function(response) {
+    get(baseUrl + "set/" + id, function (response) {
         //TODO: error handling
         /*if (!response.success) {
             $('#myModal').modal('show');
@@ -79,7 +170,7 @@ function stop() {
 }
 
 function get(url, callback) {
-    getAjax(url, function(data){ 
+    getAjax(url, function (data) {
         //TODO: remove logging
         //console.log(JSON.parse(data));
         if (callback) {
@@ -91,8 +182,8 @@ function get(url, callback) {
 function getAjax(url, success) {
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('GET', url);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState > 3 && xhr.status == 200) success(xhr.responseText);
     };
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
